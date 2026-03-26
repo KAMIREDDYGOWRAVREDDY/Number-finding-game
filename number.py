3c1f5ed10158de98f0f905b061c676be3d10c8b8
KAMIREDDYGOWRAVREDDY <kamireddygowravreddy7@gmail.com>
2026-03-26 10:27:28 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,1 +1,14 @@
+left=0
+right=100
+while left<right:
+  mid=(left+right)//2
+  print('your number is',mid)
+  ans=input('enter your answer low or high:')
 
+  if ans=='low':
+    right=mid
+  elif ans=='high':
+    left=mid
+  else:
+    print('your number is',mid)
+    break
